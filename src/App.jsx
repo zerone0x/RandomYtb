9e8b1cd073f0bc7d141e7f148a8d789c94db2d4e
zerone0x <soberzml2333@gmail.com>
2024-09-24 23:22:15 +0800

    Refactor RandomYouTubePlayer and move to components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
-import RandomYouTubePlayer from './RandomYouTubePlayer'
+import RandomYouTubePlayer from './components/RandomYouTubePlayer'
 
 function App() {
 
   return (
     <div className='App'>
-    <RandomYouTubePlayer channelId="UC2eYFnH61tmytImy1mTYvhA" apiKey="AIzaSyA1EhVH4I5HWDl-JE73xNUsR6OHZHZt9m8" />
+    <RandomYouTubePlayer />
     </div>
   )
 }
---
+++ src/components/RandomYouTubePlayer.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import { videoIds } from '../utils/data';
+
+function getRandomVideoId() {
+  return videoIds[Math.floor(Math.random() * videoIds.length)];
+}
+
+function RandomYouTubePlayer() {
+  const [videoId, setVideoId] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleClick = () => {
+      setVideoId(getRandomVideoId())
+  };
+
+  useEffect(() => {
+    // Fetch videos from the channel
+    setIsLoading(true)
+    setVideoId(getRandomVideoId())
+    setIsLoading(false)
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <section className='video-container'>
+        <iframe
+        title={
+          'Luke smith'
+        }
+        src={`https://www.youtube.com/embed/${videoId}`}
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        />
+      <button onClick={handleClick}>🎲</button>
+    </section>
+  );
+}
+
+export default RandomYouTubePlayer;
