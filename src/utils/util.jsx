68290037fce321fd122e80c1625ebfbcaf5a00bb
zerone0x <soberzml2333@gmail.com>
2024-10-04 03:18:53 +0800

    Update toast message in debounce function

--- src/utils/util.jsx
+++ src/utils/util.jsx
@@ -1,11 +1,10 @@
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { toast } from "react-toastify";
 export function debounce(func, wait) {
   let timeout;
   return function (...args) {
     const context = this;
     clearTimeout(timeout);
-    toast.info("Take a break☕");
+    toast.info("🧑‍🍳Cooking...🍳");
     timeout = setTimeout(() => func.apply(context, args), wait);
   };
 }
