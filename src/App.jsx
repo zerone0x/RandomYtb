cc32a85e2e382aa1a28550d0b0c0c88a8f051be3
zerone0x <soberzml2333@gmail.com>
2024-10-03 20:46:28 +0800

    Add routing and implement home page structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,16 @@
-import "./App.css";
-import RandomYouTubePlayer from "./components/RandomYouTubePlayer";
-import { useState } from "react";
-import { Helmet } from "react-helmet-async";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Suspense, lazy } from "react";
+import Spinner from "./components/Spinner";
+const Home = lazy(() => import("./pages/home"));
 function App() {
-  const [channel, setChannel] = useState("curb");
-  const [range, setRange] = useState("all");
   return (
-    <div className="App">
-      <Helmet>
-        <title>Random YouTube Player</title>
-        <meta
-          name="description"
-          content="Discover the Random YouTube Player of luke smith and curb your enthusiasm, offering a fun way to explore videos from all ranges. Enjoy a unique viewing experience!"
-        />
-        <meta
-          name="keywords"
-          content="luke smith, curb your enthusiasm, Random YouTube Player, YouTube player, video streaming, online video player, interactive video, random video selection, web application"
-        />
-        <meta property="og:title" content="Random YouTube Player" />
-        <meta
-          property="og:description"
-          content="Discover the Ultimate Random YouTube Player"
-        />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://randomytb.vercel.app/" />
-        <meta
-          property="og:image"
-          content="https://randomytb.vercel.app/emoji.png"
-        />
-        <meta property="og:site_name" content="Random YouTube Player" />
-      </Helmet>
-      <>
-        <h1>Random YouTube Player</h1>
-        <h2>Channels👇</h2>
-        <div className="button-container">
-          <button
-            onClick={() => {
-              setChannel("luke");
-              setRange("all");
-            }}
-            className={channel === "luke" ? "tab-active" : ""}
-            disabled={channel === "luke"}
-          >
-            Luke
-          </button>
-          <button
-            onClick={() => {
-              setChannel("curb");
-              setRange("all");
-            }}
-            className={channel === "curb" ? "tab-active" : ""}
-            disabled={channel === "curb"}
-          >
-            Curb
-          </button>
-        </div>
-        <RandomYouTubePlayer
-          channel={channel}
-          setRange={setRange}
-          range={range}
-        />
-      </>
-      <div className="footer">
-        <span>
-          issues? contact me:{" "}
-          <a href="https://github.com/zerone0x/RandomYtb/issues">
-            Github issues
-          </a>
-        </span>
-      </div>
-    </div>
+    <Suspense fallback={<Spinner />}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
   );
 }
 
---
+++ src/pages/home.jsx
@@ -0,0 +1,77 @@
+import "../App.css";
+import RandomYouTubePlayer from "../components/RandomYouTubePlayer";
+import { useState } from "react";
+import { Helmet } from "react-helmet-async";
+
+function Home() {
+  const [channel, setChannel] = useState("curb");
+  const [range, setRange] = useState("all");
+  return (
+    <div className="Home">
+      <Helmet>
+        <title>Random YouTube Player</title>
+        <meta
+          name="description"
+          content="Discover the Random YouTube Player of luke smith and curb your enthusiasm, offering a fun way to explore videos from all ranges. Enjoy a unique viewing experience!"
+        />
+        <meta
+          name="keywords"
+          content="luke smith, curb your enthusiasm, Random YouTube Player, YouTube player, video streaming, online video player, interactive video, random video selection, web application"
+        />
+        <meta property="og:title" content="Random YouTube Player" />
+        <meta
+          property="og:description"
+          content="Discover the Ultimate Random YouTube Player"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://randomytb.vercel.app/" />
+        <meta
+          property="og:image"
+          content="https://randomytb.vercel.app/emoji.png"
+        />
+        <meta property="og:site_name" content="Random YouTube Player" />
+      </Helmet>
+      <>
+        <h1>Random YouTube Player</h1>
+        <h2>Channels👇</h2>
+        <div className="button-container">
+          <button
+            onClick={() => {
+              setChannel("luke");
+              setRange("all");
+            }}
+            className={channel === "luke" ? "tab-active" : ""}
+            disabled={channel === "luke"}
+          >
+            Luke
+          </button>
+          <button
+            onClick={() => {
+              setChannel("curb");
+              setRange("all");
+            }}
+            className={channel === "curb" ? "tab-active" : ""}
+            disabled={channel === "curb"}
+          >
+            Curb
+          </button>
+        </div>
+        <RandomYouTubePlayer
+          channel={channel}
+          setRange={setRange}
+          range={range}
+        />
+      </>
+      <div className="footer">
+        <span>
+          issues? contact me:{" "}
+          <a href="https://github.com/zerone0x/RandomYtb/issues">
+            Github issues
+          </a>
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
