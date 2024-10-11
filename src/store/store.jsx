3fe5551e2b683bbc341a9ae455d75291d242a841
zerone0x <soberzml2333@gmail.com>
2024-10-12 03:28:13 +0800

    Remove unused components and application structure

--- src/store/store.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import channelReducer from "../features/channel/channelSlice";
-
-export const store = configureStore({
-  reducer: {
-    channel: channelReducer,
-  },
-});
